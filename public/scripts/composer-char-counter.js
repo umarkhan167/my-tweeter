005628ba51638c9b9069b80415f596c9f4473280
Muhammad Khan <UmarP@Umars-MacBook-Pro.local>
2021-12-08 15:52:11 -0500

    Add character counter function to the tweet text area

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+
+
+$(document).ready(function() {
+  // --- our code goes here ---
+ const maxLength = 140;
+ const tweetArea = $('#tweet-area');
+ const counter = $('#counter');
+
+ $(tweetArea).on('input', function() {
+   const characters = $(this).val().length;
+   let remainingChars = maxLength - characters;
+   counter.text(remainingChars);
+   if (remainingChars < 0) {
+     counter.addClass("red");
+   } else {
+     counter.removeClass("red");
+   }
+ });
+});
