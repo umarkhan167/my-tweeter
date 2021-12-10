598acc83ca4c4787704f67158bd0f13c4989f3bc
Muhammad Khan <UmarP@Umars-MacBook-Pro.local>
2021-12-10 16:20:11 -0500

    Add jquery functions for dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 // {/* <img src=${user.avatars}/> */}
 function createTweetElement(tweetObject) {
@@ -38,15 +51,20 @@ function createTweetElement(tweetObject) {
                     </div>
                   </article>`
     return tweet
-  }
+}
 
-const $tweetElement = createTweetElement(tweetData)
-console.log($tweetElement); // to see what it looks like
-  
+
+
 $(document).ready(function() {
   // $("tag-name")-this targets all the tag names
   // $("#tweet")-this targets the singular tag element that has the same id name- <div id="tweet">
   // $(".name")-this targets all the tag elements that have the class name- <div class="name">
-  $("#tweets-container").append($tweetElement);
+  function renderTweets(tweetsData) {
+    for (let tweet of tweetsData) {
+      const postedTweet = createTweetElement(tweet);
+      $("#tweets-container").append(postedTweet);
+    }
+  }
+  renderTweets(data);
 });
 
